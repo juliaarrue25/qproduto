90c43fa205f349636f7f2b98a0d1f63ae1e34000
JULIA ARONE ARRUE <14104940@pucrs.br>
2019-10-07 22:11:15 -0300

    Primeiro commit

---
+++ trabalho2.1/src/main/java/trabalho2/pucrs/Leitura.java
@@ -0,0 +1,48 @@
+package trabalho2.pucrs;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+
+public class Leitura {
+	
+	
+	public void leitura() {
+		try {
+			ArrayList<ResultFile> result = new ArrayList<ResultFile>();
+			FileReader ler = new FileReader("calculos.txt");
+			BufferedReader reader = new BufferedReader(ler);
+			String linha;
+			Double somatorio = new Double("0");
+			while((linha = reader.readLine()) != null) {
+				String[] numeros = linha.split(":");
+			ResultFile resultFile = new ResultFile(
+					Double.valueOf(numeros[0]),
+					Double.valueOf(numeros[1]),
+					Double.valueOf(numeros[2]));
+			
+			result.add(resultFile);
+			}
+			
+			for(ResultFile resultFile: result) {
+				
+				somatorio = somatorio + resultFile.getCubo();
+			}
+			
+			System.out.println(somatorio);
+			
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ trabalho2.1/src/main/java/trabalho2/pucrs/ResultFile.java
@@ -0,0 +1,49 @@
+package trabalho2.pucrs;
+
+public class ResultFile {
+	
+ private Double numero;
+ private Double quadrado;
+ private Double cubo;
+ 
+ 
+public ResultFile(Double numero, Double quadrado, Double cubo) {
+	this.numero = numero;
+	this.quadrado = quadrado;
+	this.cubo = cubo;
+}
+
+
+public Double getNumero() {
+	return numero;
+}
+
+
+public void setNumero(Double numero) {
+	this.numero = numero;
+}
+
+
+public Double getQuadrado() {
+	return quadrado;
+}
+
+
+public void setQuadrado(Double quadrado) {
+	this.quadrado = quadrado;
+}
+
+
+public Double getCubo() {
+	return cubo;
+}
+
+
+public void setCubo(Double cubo) {
+	this.cubo = cubo;
+} 
+
+
+ 
+ 
+}
